8d2a8f5ac49c5629f293e530d45d822fcfd67aad
Arijit Roy <roy.arijit2001@gmail.com>
2021-09-27 21:17:16 +0530

    added utililty functions for stratified split :package:

--- amethyst/dataloader/data_utils.py
+++ amethyst/dataloader/data_utils.py
@@ -1,7 +1,8 @@
+from typing import List, Optional, Type
+
 import pandas as pd
 import numpy as np
 import math
-
 
 
 def verify_split_ratio(ratio: float) -> float:
@@ -14,9 +15,50 @@ def verify_split_ratio(ratio: float) -> float:
         ratio if value is acceptable, else raises ValueError
     """
     if not isinstance(ratio, float):
-        raise ValueError("Expected ratio to be of type float")
+        raise TypeError("Expected ratio to be of type float")
 
     if ratio <= 0 or ratio >= 1:
         raise ValueError("Split ratio should be in between 0 and 1")
     
     return ratio
+
+
+def verify_stratification(
+    data: pd.DataFrame,
+    ratio: float,
+    user_col: str,
+    item_col: str,
+    filter_col: str,
+    min_rating_count: Optional[int] = 1,
+    seed: Optional[int] = 11
+):
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError("Expected data to be of type pandas.DataFrame")
+    
+    verify_split_ratio(ratio)
+
+    if user_col not in data.columns:
+        raise ValueError(f"Column: {user_col} not in data")
+    
+    if item_col not in data.columns:
+        raise ValueError(f"Column: {item_col} not in data")
+
+    if filter_col != "user" and filter_col != "item":
+        raise ValueError("Expected filter_col to be either user or item")
+
+    if min_rating_count < 1:
+        raise ValueError("min_rating_count should be greater than or equal to 1")
+
+
+def ratio_split(data: pd.DataFrame, ratios: List[float], shuffle: Optional[bool]=False, seed: Optional[int]=42):
+    indices = np.cumsum(ratios).tolist()[: -1]
+
+    if shuffle:
+        data = data.sample(frac=1, random_state=seed)
+
+    splits = np.split(data, [round(x * len(data)) for x in indices])
+
+    for i in range(len(ratios)):
+        splits[i]["split_index"] = i
+    
+    return splits
